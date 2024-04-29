e231e3658dfedfe922e23595db6e44fcf27e4892
maxi-gartner <gartner.maxi@hotmail.com>
2024-04-29 03:09:04 -0300

    estilo de index terminado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,14 +7,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    <title>Sistema de Ventas</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
@@ -28,10 +28,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
+            <a href="index3.html" style="color: white" class="nav-link">Inicio</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
+            <a href="#" style="color: white" class="nav-link">Contacto</a>
         </li>
         </ul>
 
@@ -159,79 +159,131 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <aside class="main-sidebar sidebar-dark-primary elevation-4 d-flex flex-column">
+        <a href="{{url('/')}}" class="brand-link">
+        <img src="{{asset('img/pngwing.com.png')}}" alt="Sistema de ventas Logo" class="brand-image img-circle" style="opacity: .8">
+        <span class="brand-text font-weight-light">Sistema de ventas</span>
         </a>
 
-        <!-- Sidebar -->
         <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
             <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="#" class="d-block">{{ Auth::user()->name }}</a>
             </div>
         </div>
 
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-                <button class="btn btn-sidebar">
-                <i class="fas fa-search fa-fw"></i>
-                </button>
-            </div>
-            </div>
-        </div>
-
-        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-                with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
-                <a href="#" class="nav-link active">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-users"></i>
                 <p>
-                    Starter Pages
+                    Usuarios
                     <i class="right fas fa-angle-left"></i>
                 </p>
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active">
+                    <a href="paginas_usuarios/usuarios.php" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
-                    <p>Active Page</p>
+                    <p>Lista de Usuarios</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="paginas_usuarios/create_user.php" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
-                    <p>Inactive Page</p>
+                    <p>Creacion de usuarios</p>
                     </a>
                 </li>
                 </ul>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
+                <i class="nav-icon fas fa-address-book"></i>
                 <p>
-                    Simple Link
-                    <span class="right badge badge-danger">New</span>
+                    Roles
+                    <i class="right fas fa-angle-left"></i>
                 </p>
                 </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="paginas_roles/roles.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Lista de Roles</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="paginas_roles/create_roles.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Creacion de rol</p>
+                    </a>
+                </li>
+                </ul>
             </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tags"></i>
+                <p>
+                    Productos
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="paginas_almacen/productos.php" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Lista de Productos</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Agregar Producto</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-layer-group"></i>
+                <p>
+                    Categorías
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="paginas_categorias/categorias.php" class="nav-link-logout">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Lista de Categorías</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Creacion de Categorías</p>
+                    </a>
+                </li>
+                </ul>
+            </li>
+            <li class="nav-item logout-li" >
+                <a class="nav-link logout-button" 
+                    href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                <i class="nav-icon fas fa-door-closed"></i>
+                <p>
+                    Cerrar Sesion
+                </p>
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+            </li>
+
             </ul>
         </nav>
-        <!-- /.sidebar-menu -->
         </div>
-        <!-- /.sidebar -->
     </aside>
 
     <!-- Content Wrapper. Contains page content -->
@@ -241,27 +293,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="container-fluid">
             <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Starter Page</h1>
+                <!-- <h1 class="m-0">Navegacion</h1> -->
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Starter Page</li>
+                <li class="breadcrumb-item active"><a href="{{ url('/') }}">Sistema Integrado de Gestión Comercial</a></li>
+                <li class="breadcrumb-item ">Starter Page</li>
                 </ol>
             </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
         </div>
         <!-- /.content-header -->
-
-        <!-- Main content -->
-        <div class="content-wrapper">
-        <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-            </div>
-        </div>
-        </div>
         <!-- Main content -->
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <div class="container">
@@ -453,12 +496,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- Main Footer -->
     <footer class="main-footer">
-        <!-- To the right -->
-        <div class="float-right d-none d-sm-inline">
-        Anything you want
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        All rights reserved &copy; Gartner Maximiliano
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2024-2025 <a href="https://adminlte.io">Sistema de ventas</a></strong> 
     </footer>
 </div>
 <!-- ./wrapper -->
@@ -466,10 +509,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
