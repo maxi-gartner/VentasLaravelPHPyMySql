26420487fa94e191854b7c5c1dd6ed7cbe1d3f50
maxi-gartner <gartner.maxi@hotmail.com>
2024-05-04 18:24:59 -0300

    :tada: cambiando formato

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () { return view('index'); })->middleware('auth');
-Route::get('/users', function () { return view('pages_user.users'); })->middleware('auth');
-Route::get('/users/create', function () { return view('pages_user.create_user'); })->middleware('auth');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('index');
+});
