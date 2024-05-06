0465992a9b602e4b85bb818c847df812d578b535
maxi-gartner <gartner.maxi@hotmail.com>
2024-05-06 03:17:46 -0300

    :sparkles: agregando controladores y middlewares

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+/* Route::get('/', function () {
+    return view('home');
+}); */
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Route::get('/', function () { return view('admin'); });
+
+
+Route::get('/', [App\Http\Controllers\UsersReadController::class, 'index'])->name('home')->middleware('auth');
