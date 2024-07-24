f40eef89172e5a22c2357b5ef6d4bb2f8891aff2
maxi-gartner <gartner.maxi@hotmail.com>
2024-07-24 13:44:44 -0300

    migrando a Laravel

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/', function () {
-    return view('home');
-}); */
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-//Route::get('/', function () { return view('admin'); });
-
-
-Route::get('/', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
-Route::get('/users', [App\Http\Controllers\UsersReadController::class, 'index'])->name('users.index')->middleware('auth');
+Route::get('/', function () {
+    return view('welcome');
+});
