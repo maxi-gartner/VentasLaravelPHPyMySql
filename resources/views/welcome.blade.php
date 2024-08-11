68ca2fd30c79b559bd92b805380f5bdfa02bcdd3
maxi-gartner <gartner.maxi@hotmail.com>
2024-08-10 21:47:35 -0300

    10/8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,7 +98,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                Control de Stock
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
